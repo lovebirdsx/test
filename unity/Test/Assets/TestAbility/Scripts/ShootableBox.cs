110f5020725b295514df0f1526ff3b049f68d847
XiaoXiao <lovebirdsx@gmail.com>
2018-06-06 21:51:13 +0800

    add Unity test

---
+++ unity/Test/Assets/TestAbility/Scripts/ShootableBox.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace TestAblity
+{
+    public class ShootableBox : MonoBehaviour
+    {
+        public float health = 100;
+        public void Damage(int damg)
+        {
+            health -= damg;
+        }
+    }
+}
